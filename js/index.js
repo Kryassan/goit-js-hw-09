07eb0d5ed07a7e1211bb83319441871d9d4ae864
Viacheslav Panasiuk <laburunt@gmail.com>
2021-09-22 23:17:34 +0300

    final

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import galleryItems from './galleryItems.js';
 
+
 const listEl = document.querySelector('.js-gallery');
 const lightboxEl = document.querySelector('.lightbox');
 const originalImageEl = document.querySelector('.lightbox__image');
@@ -25,6 +26,8 @@ const addListElements = galleryItems.map(itemEl).join('');
 
 listEl.insertAdjacentHTML('beforeend', addListElements);
 
+// modal
+
 listEl.addEventListener('click', onOpenModal);
 
 function onOpenModal(e) {
@@ -39,4 +42,27 @@ function onCloseModal() {
   lightboxEl.classList.remove('is-open');
   originalImageEl.src = '';
 }
+
+// slider
+window.addEventListener('keydown', slideGallery);
+
+function slideGallery(e) {
+  let nextIndex = 0;
+  const currentIndex = galleryItems.indexOf(
+    galleryItems.find(i => i.description === originalImageEl.alt),
+  );
+  
+  if (e.key == 'ArrowRight') {
+    nextIndex = currentIndex < galleryItems.length - 1 ? currentIndex + 1 : 0;
+  }
+  if (e.key == 'ArrowLeft') {
+    nextIndex = currentIndex > 0 ? currentIndex - 1 : galleryItems.length - 1;
+  }
+
+  originalImageEl.src = galleryItems[nextIndex].original;
+  originalImageEl.alt = galleryItems[nextIndex].description;
+}
+
+
+
 
